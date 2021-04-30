54cf0daecf7c0569482bd1f4cc55f04ee9f8e971
khai.phv <khai.phv@gmail.com>
2021-04-30 19:00:50 +0700

    fix user pop up error

--- src/components/homepage/sidebar/SideBar.jsx
+++ src/components/homepage/sidebar/SideBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SideBarItem from "./SideBarItem";
 import sideBarListItems from "./sideBarListItems.js";
 import UserPopUp from "./UserPopUp";
@@ -6,19 +6,27 @@ import './SideBar.css';
 
 function SideBar() {
   // show pop up
-  function displayPopUp() {
-      document.getElementById("popup").classList.add("show");
-      document.querySelector(".new-feed").style.zIndex = "-1";
-      document.getElementById("user-test").style.zIndex = "-1";
-  }
-  // Remove pop up
-  document.body.addEventListener("click", function () {
-    if (document.getElementById("popup").classList.contains('show')) {
-      document.getElementById("popup").classList.remove("show");
-      document.querySelector(".new-feed").style.zIndex = "0";
-      document.getElementById("user-test").style.zIndex = "0";
+  const [isDisplay, setIsDisplay] = useState(false);
+  useEffect(() => {
+    function TooglePopUp() {
+      if (!isDisplay) {
+        setIsDisplay(true);
+        document.getElementById("user-test").style.zIndex = "-1";
+
+      }
+      // else {
+      //   document.getElementById("popup").classList.remove("show");
+      //   setIsDisplay(false);
+      // }
     }
-  }, true);
+    document.getElementById("user-test").addEventListener("click", TooglePopUp);
+    isDisplay &&
+      document.body.addEventListener("click", function() {
+        document.getElementById("user-test").style.zIndex = "0";
+        setIsDisplay(false);
+      }, true);
+  })
+  // document.querySelector(".new-feed").style.zIndex = "-1";
   return (
     <div id="side-bar">
       <div className="side-bar d-flex d-flex-column js-btw">
@@ -43,8 +51,8 @@ function SideBar() {
           <button className="btn w-90 btn-twitter">Tweet</button>
         </div>
         <div className="trigger">
-          <UserPopUp />
-          <a onClick={displayPopUp} id="user-test" className="bottom-side-bar d-flex align-bl js-btw">
+          {isDisplay && <UserPopUp />} 
+          <a id="user-test" className="bottom-side-bar d-flex align-bl js-btw">
             <div className=" d-flex align-bl">
               <i class="fas fa-user"></i>
               <div>
