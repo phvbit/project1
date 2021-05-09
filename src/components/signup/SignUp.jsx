b94e3580c574da0118de95757e5ebe87310ae0f9
khai.phv <khai.phv@gmail.com>
2021-05-09 15:26:03 +0700

    fix typo err

--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -3,6 +3,7 @@ import './SignUp.css';
 import monthList from "./date";
 import yearList from "./year";
 
+
 function SignUp() {
   const [useInstead, setInstead] = useState("true");
   function UseInstead() {
@@ -10,7 +11,7 @@ function SignUp() {
   }
 
   const [isLeapYear, setIsLeapYear] = useState(false);
-  function ChangYear(event) {
+  function ChangeYear(event) {
     const year = event.target.value;
     if (year % 400 === 0) {
       setIsLeapYear(true);
@@ -53,12 +54,13 @@ function SignUp() {
     date ++;
   }
 
+
   return (
     <div className="signup-container d-flex js-center align-bl">
       <div className="create-new">
         <div className="d-flex align-bl justify-content-end">
           <i className="fab fa-twitter login-icon"></i>
-          <button id="btn-next" disabled className="btn btn-primary btn-create">Next</button>
+          <a id="btn-next" className="btn-primary">Next</a>
         </div>
         <h3>Create your account</h3>
         <div className="form-floating">
@@ -70,6 +72,9 @@ function SignUp() {
           <label className="form-label" htmlFor="create-phone">{useInstead ? "Phone" : "Email"}</label>
         </div>
         <p style={{color: "#1DA1F2"}} onClick={UseInstead} id="use-instead" href="#">{useInstead ? "Use email instead" : "Use phone instead"}</p>
+
+
+        {/* Date select */}
         <strong><p>Date of birth</p></strong>
         <p>This will not be shown publicly. Confirm your own age, even if this account is for a business, a pet, or something else.</p>
         <div className="birth-sel d-flex">
@@ -93,7 +98,7 @@ function SignUp() {
             <label className="form-label" htmlFor="date-sel">Date</label>
           </div>
           <div className="form-floating">
-            <select onChange={ChangYear} className="form-select" name="year" id="#year-sel">
+            <select onChange={ChangeYear} className="form-select" name="year" id="#year-sel">
               <option value="">Select Year</option>
               { yearList.map(year => (
                 <option key={year} value={year}>{year}</option>
